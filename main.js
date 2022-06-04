cadd259153f1e89ffb0f42ca1f422a661ce3eefa
Advika Tyagi <78788299+Advika123456789@users.noreply.github.com>
2022-06-04 12:40:23 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,8 +1,13 @@
 song= "";
+leftWristX = 0;
+leftWristY = 0;
+rightWristX = 0;
+rightWristY = 0;
+
 
 
 function preload() {
-     song = loadSound("music.mp3");
+     song = loadSound("Undertale Shop.mp3");
 }
 
 function setup() {
@@ -11,6 +16,9 @@ function setup() {
 
     video = createCapture(VIDEO);
     video.hide();
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
 }
 
 function draw() {
@@ -20,4 +28,24 @@ function draw() {
 
 function play() {
     song.play();
+    song.setVolume(1);
+    song.rate(1);
+}
+
+function modelLoaded() {
+    console.log('PoseNet Is Intialized!');
+}
+
+function gotPoses(results) {
+    if(results.length > 0) {
+        console.log(results);
+        leftWristX = results[0].pose.leftWrist.x;
+        leftWristY = results[0].pose.leftWrist.y;
+        console.log("leftWristX = " + leftWristX + " leftWristY = " + leftWristY);
+
+        rightWristX = results[0].pose.rightWrist.x;
+        rightWristY = results[0].pose.rightWrist.y;
+        console.log("rightWristX = " + rightWristX +" rightWristY = " + rightWristY);
+
+    }
 }
