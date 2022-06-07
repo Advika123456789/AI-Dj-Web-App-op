bbc83d5d3c6b9dc90df3d188cad065deb34b9dc3
Advika Tyagi <78788299+Advika123456789@users.noreply.github.com>
2022-06-07 13:43:06 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -21,8 +21,40 @@ function setup() {
     poseNet.on('pose', gotPoses);
 }
 
+function gotPoses(results) {
+    if(results.length > 0)
+     {
+        console.log(results);
+        scoreLeftWrist = results[0].pose.keypoints[9].score;
+        console.log("scoreLeftWrist = " + scoreLeftWrist);
+
+        leftWristX = results[0].pose.leftWrist.x;
+        leftWristY = results[0].pose.leftWrist.y;
+        console.log("leftWristX = " + leftWristX + " leftWristY = " + leftWristY);
+
+        rightWristX = results[0].pose.rightWrist.x;
+        rightWristY = results[0].pose.rightWrist.y;
+        console.log("rightWristX = " + rightWristX +" rightWristY = " + rightWristY);
+
+    }
+}
+
 function draw() {
     image(video, 0,0, 600, 500);
+
+    fill("#FF0000");
+    stroke("#FF0000");
+
+    if(scoreLeftWrist > 0.2)
+    {
+    circle(leftWristX, leftWristY, 20 );
+    InNumberleftWristY = Number(leftWristY);
+    remove_decimals = floor(InNumberleftWristY);
+    leftWristY_divide_1000 = remove_decimals/1000;
+    volume = leftWristY_divide_1000 *2 ;
+    document.getElementById("volume").innerHTML = "Volume = " + volume;
+    song.setVolume(volume);
+    }
 }
 
 
@@ -36,16 +68,3 @@ function modelLoaded() {
     console.log('PoseNet Is Intialized!');
 }
 
-function gotPoses(results) {
-    if(results.length > 0) {
-        console.log(results);
-        leftWristX = results[0].pose.leftWrist.x;
-        leftWristY = results[0].pose.leftWrist.y;
-        console.log("leftWristX = " + leftWristX + " leftWristY = " + leftWristY);
-
-        rightWristX = results[0].pose.rightWrist.x;
-        rightWristY = results[0].pose.rightWrist.y;
-        console.log("rightWristX = " + rightWristX +" rightWristY = " + rightWristY);
-
-    }
-}
